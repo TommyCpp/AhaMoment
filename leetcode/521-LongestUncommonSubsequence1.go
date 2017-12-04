e1d72207305d8c67846ad9a8ab00bbc8371cae43
Zhongyang.Wu <a444529216@hotmail.com>
2017-12-04 17:54:09 +0800

    Create 521-LongestUncommonSubsequence1.go

---
+++ leetcode/521-LongestUncommonSubsequence1.go
@@ -0,0 +1,11 @@
+func findLUSlength(a string, b string) int {
+    if(a == b){
+        return -1
+    }else{
+        if(len(a) > len(b)){
+            return len(a)
+        }else{
+            return len(b)
+        }
+    }
+}
