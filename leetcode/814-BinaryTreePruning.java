f8db1d0f69efa17a6ebf6e88c8fcc1a3cb2c9a1d
Zhongyang.Wu <zhongyang.wu@outlook.com>
2018-04-09 22:58:02 +0800

    Create 814-BinaryTreePruning.java

---
+++ leetcode/814-BinaryTreePruning.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        if(root.left != null || root.right != null){
+            root.left = this.pruneTree(root.left);
+            root.right = this.pruneTree(root.right);
+        }
+        if(root.val == 0 && root.left == null && root.right == null){
+            return null;
+        }
+        else{
+            return root;
+        }
+        
+    }
+}
