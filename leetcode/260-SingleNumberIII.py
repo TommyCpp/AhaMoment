9beb063967dee59abb23f476a197dd7d29402c9e
Zhongyang.Wu <zhongyang.wu@outlook.com>
2018-05-09 18:34:33 +0800

    Update 260-SingleNumberIII.py

--- leetcode/260-SingleNumberIII.py
+++ leetcode/260-SingleNumberIII.py
@@ -1,3 +1,4 @@
+# Solution 1, use Counter API
 import collections
 class Solution:
     def singleNumber(self, nums):
@@ -8,3 +9,24 @@ class Solution:
         counter = collections.Counter(nums)
         return [ i[0] for i in counter.most_common() if i[1] == 1]
         
+
+# Solution 2, use bit manipulation       
+import collections
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        diff = 0
+        for i in nums:
+            diff ^= i
+        diff &= -diff
+        res_a = 0
+        res_b = 0
+        for i in nums:
+            if diff & i == 0:
+                res_a ^= i
+            else:
+                res_b ^= i
+        return [res_a,res_b]
