b32440ad93b912a077effe364a71fafb6013ba6d
Zhongyang.Wu <zhongyang.wu@outlook.com>
2018-07-03 20:19:03 +0800

    Create 387-FirstUniqueCharacterInAString.py

---
+++ leetcode/387-FirstUniqueCharacterInAString.py
@@ -0,0 +1,20 @@
+
+import collections
+class Solution:
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = len(s)
+        counter = collections.Counter(s)
+        elements = counter.most_common()
+        for i in range(len(elements)-1, -1,-1):
+            if elements[i][1] == 1:
+                index = s.index(elements[i][0])
+                if index < res:
+                    res = index
+        if res == len(s):
+            return -1
+        else:
+            return res
