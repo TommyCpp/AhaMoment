d05db6989630b34297fc6425a0a25275f391064e
zhongyang.wu <zhongyang.wu@outlook.com>
2020-01-08 14:08:41 -0500

    Building block #2 of PingCAP talent plan See https://github.com/pingcap/talent-plan/blob/master/rust/projects/project-2/ [WIP]Use Log pointer to store data in memory

--- rust/kvs/src/kv.rs
+++ rust/kvs/src/kv.rs
@@ -1,14 +1,15 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, BTreeMap};
 use crate::{Result, Error};
 use std::path::Path;
-use std::fs as fs;
+use std::{fs as fs, io};
 use std::fs::{File, OpenOptions};
 use serde::{Serialize, Deserialize};
-use std::io::{Write, Read, LineWriter};
+use std::io::{Write, Read, LineWriter, BufReader, SeekFrom, BufRead, Seek, BufWriter};
 use std::ops::Add;
 
 pub struct KvStore {
-    hashmap: HashMap<String, String>,
+    data_map: BTreeMap<String, CommandLog>,
+    file_readers: HashMap<u32, BufReaderWithPos>,
     log: String, //log file path
 }
 
@@ -32,42 +33,31 @@ impl KvStore {
     pub fn open(path: &Path) -> Result<KvStore> {
         let log_file = path.join("log.txt");
         let log_file_str = log_file.to_str().unwrap();
-        if !log_file.exists() {
-            //only create if not exists
-            match File::create(log_file_str) {
-                Err(why) => panic!("{}", why),
-                _ => ()
-            }
-            Ok(KvStore {
-                hashmap: HashMap::new(),
-                log: String::from(log_file_str),
-            })
-        } else {
-            let mut store = KvStore {
-                hashmap: HashMap::new(),
-                log: String::from(log_file_str),
-            };
-            store.read_log()?;
-            Ok(store)
-        }
+        let mut store = KvStore {
+            data_map: BTreeMap::new(),
+            file_readers: HashMap::new(),
+            log: String::from(log_file_str),
+        };
+        store.read_log()?;
+        Ok(store)
     }
 
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
-        match self.hashmap.get(&key) {
-            Some(val_ref) => Ok(Some(String::from(val_ref))),
+        match self.data_map.get(&key) {
+            Some(val_ref) => Ok(None),
             None => Ok(None),
         }
     }
 
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        let (key_cp, value_cp) = (key.clone(), value.clone());
-        let _ = self.hashmap.insert(key, value); //ignore the return value
-        self.append_log(Log::Set { key: key_cp, value: value_cp });
+//        let (key_cp, value_cp) = (key.clone(), value.clone());
+//        let _ = self.data_map.insert(key, value); //ignore the return value
+//        self.append_log(Log::Set { key: key_cp, value: value_cp });
         Ok(())
     }
 
     pub fn remove(&mut self, key: String) -> Result<()> {
-        match self.hashmap.remove(&key) {
+        match self.data_map.remove(&key) {
             Some(val) => {
                 self.append_log(Log::Remove { key });
                 Ok(())
@@ -89,28 +79,28 @@ impl KvStore {
     }
 
     fn read_log(&mut self) -> Result<()> {
-        let mut file = self.open_file(true)?;
-        let mut s = String::new();
-        match file.read_to_string(&mut s) {
-            Ok(us) => {
-                let mut lines: Vec<&str> = s.split('\n').collect();
-                for line in lines {
-                    if line.len() != 0 {
-                        let log: Log = serde_json::from_str(line).unwrap();
-                        match log {
-                            Log::Set { key, value } => {
-                                self.hashmap.insert(key, value);
-                            }
-                            Log::Remove { key } => {
-                                self.hashmap.remove(&key);
-                            }
-                            Log::Get { key } => {}
-                        }
-                    }
-                }
-            }
-            Err(why) => panic!("Cannot read file, {}", why)
-        }
+//        let mut file = self.open_file(true)?;
+//        let mut s = String::new();
+//        match file.read_to_string(&mut s) {
+//            Ok(us) => {
+//                let mut lines: Vec<&str> = s.split('\n').collect();
+//                for line in lines {
+//                    if line.len() != 0 {
+//                        let log: Log = serde_json::from_str(line).unwrap();
+//                        match log {
+//                            Log::Set { key, value } => {
+//                                self.data_map.insert(key, value);
+//                            }
+//                            Log::Remove { key } => {
+//                                self.data_map.remove(&key);
+//                            }
+//                            Log::Get { key } => {}
+//                        }
+//                    }
+//                }
+//            }
+//            Err(why) => panic!("Cannot read file, {}", why)
+//        }
         Ok(())
     }
 
@@ -125,5 +115,78 @@ impl KvStore {
             Err(why) => panic!("could not open file, {}", why),
             Ok(file) => Ok(file)
         }
+    }
+}
+
+#[derive(Copy, Clone)]
+struct CommandLog {
+    file_id: u64,
+    pos: u64,
+    len: u64,
+}
+
+struct BufReaderWithPos {
+    reader: BufReader<File>,
+    pos: u64,
+}
+
+impl BufReaderWithPos {
+    fn new(mut file: File) -> Result<Self> {
+        let pos = file.seek(SeekFrom::Current(0))?;
+        let reader = BufReader::new(file);
+        Ok(BufReaderWithPos {
+            reader,
+            pos,
+        })
+    }
+}
+
+impl Read for BufReaderWithPos {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let len = self.reader.read(buf)?;
+        self.pos += len as u64;
+        Ok(len)
+    }
+}
+
+impl Seek for BufReaderWithPos {
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
+        self.pos = self.reader.seek(pos)?;
+        Ok(self.pos)
+    }
+}
+
+struct BufWriterWithPos {
+    writer: BufWriter<File>,
+    pos: u64,
+}
+
+impl BufWriterWithPos {
+    fn new(mut file: File) -> Result<Self> {
+        let pos = file.seek(SeekFrom::Current(0))?;
+        let writer = BufWriter::new(file);
+        Ok(BufWriterWithPos {
+            writer,
+            pos,
+        })
+    }
+}
+
+impl Write for BufWriterWithPos {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let len = self.writer.write(buf)?;
+        self.pos += len as u64;
+        Ok(len)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.writer.flush()
+    }
+}
+
+impl Seek for BufWriterWithPos {
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
+        self.pos = self.writer.seek(pos)?;
+        Ok(self.pos)
     }
 }
